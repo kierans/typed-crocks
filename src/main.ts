3549455600a5d038cd450cb198e44468d74d6490
Kieran Simpson <kierans777@gmail.com>
2021-01-21 15:56:10 +1100

    Add example usage of Crocks.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
-console.log("Hello World");
+/*
+ * This import compiles, but at runtime the module isn't found/loaded.
+ *
+ * See the transpiled JS (npm run build)
+ */
+// import Async from "../types/crocks/Async";
+
+// does not compile
+import Async from "crocks/Async";
+
+const a = Async.of("Hello World");
+
+a.fork(
+	() => console.log("Error!"),
+	console.log
+);
